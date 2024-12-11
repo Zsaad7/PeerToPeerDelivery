47733de6753595b1e02f1c73d5c65d7d44a3bd50
Zsaad7 <saadzridi25@gmail.com>
2024-12-12 00:16:39 +0100

    Models : Payment Class Update

--- Models/Payment.cs
+++ Models/Payment.cs
@@ -2,6 +2,15 @@
 {
     public class Payment
     {
-        /// q definir apres
+        /// q definir apr
+        public int IdPayement { get; set; }
+        public decimal Montant { get; set; }
+        public DateTime DatePaiement { get; set; }
+        public string ModePaiement { get; set; } = string.Empty; // Carte de crédit, virement, etc.
+        public string ReferencePaiement { get; set; } = string.Empty; // Numéro de transaction, etc.
+        public string Etat { get; set; } = string.Empty;
+
+
+
     }
 }
