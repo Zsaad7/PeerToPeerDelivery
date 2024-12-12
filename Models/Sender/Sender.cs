aad1f292a64ae441ab61c55f30fa5f90457c3e2b
Zsaad7 <saadzridi25@gmail.com>
2024-12-12 01:49:23 +0100

    Views : Version 1, Registration

--- Models/Sender/Sender.cs
+++ Models/Sender/Sender.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Peer2PeerDelivery.Models.Carrier;
+using System.ComponentModel.DataAnnotations;
 using System.Numerics;
 
 namespace Peer2PeerDelivery.Models.Sender
@@ -9,13 +10,12 @@ namespace Peer2PeerDelivery.Models.Sender
         public int IdSender { get; set; }
         public string FirstName { get; set; } = string.Empty;
         public string SecondName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-        public string Number { get; set; } = string.Empty;
+        public string NumberPhone { get; set; } = string.Empty;
         public string Address { get; set; } = string.Empty;
-        public string Phone { get; set; } = string.Empty;
         public string ProfilePicture { get; set; } = string.Empty;
-        public string Note { get; set; } = string.Empty;
-
+        public ICollection<Review>? reviews { get; set; }
         public ICollection<Item>? Items { get; set; }
         public ICollection<Payment>? payments { get; set; }
 
